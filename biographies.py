0cd303238428e9b3a2fdf9aba7353f1916b58d0b
kkbaysa <57466485+kkbaysa@users.noreply.github.com>
2021-11-03 16:52:05 -0500

    adding labels and input boxes for forms

---
+++ biographies.py
@@ -0,0 +1,12 @@
+def biography(nameIn, addressIn, cityIn, stateIn, ZIPIn, specialtyIn, emailIn, phoneNumberIn, storyIn):
+    return {
+        name: nameIn,
+        address: addressIn,
+        city: cityIn,
+        state: stateIn,
+        ZIP: ZIPIn,
+        specialty: specialtyIn,
+        email: emailIn,
+        phone: phoneNumberIn,
+        story: storyIn
+    }
